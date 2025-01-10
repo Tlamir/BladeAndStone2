ac9e1b4b14e3afc8ac9f317927d9fe528f28f061
Yigithan Guven <demirejderha@gmail.com>
2025-01-11 00:50:32 +0300

    fix: Fixed camera movement with player

--- sources/scenes/GameScene/GameScene.cpp
+++ sources/scenes/GameScene/GameScene.cpp
@@ -20,7 +20,7 @@ std::unique_ptr<Player> GameScene::player = nullptr;
 std::unique_ptr<b2World> GameScene::world = nullptr;
 
 Vector2 characterPosition2 = { AppConstants::ScreenWidth / 2, AppConstants::ScreenHeight / 2 }; // Initial position
-float characterSpeed2 = 20.0f;            // Movement speed
+float characterSpeed2 = 26.6f;            // Movement speed //Adjust CameraSpeed to character speed later
 
 // Camera system
 Camera2D camera2 = { 0 };
